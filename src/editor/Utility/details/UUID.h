aaff87f15c9a857a4a1fa80271b00ca16347810d
Olivia Heartfelt <heartfelt.olivia@gmail.com>
2026-08-16 01:32:31 +0200

    update UUID.h utility

--- src/editor/Utility/details/UUID.h
+++ src/editor/Utility/details/UUID.h
@@ -13,5 +13,18 @@ namespace UUID {
             std::memcpy(&id, bytes.constData(), 16);
             return id;
         }
+
+        static void toDataStream(QDataStream& out, const muuid::uuid& id) {
+            out.writeRawData(reinterpret_cast<const char*>(id.bytes.data()), 16);
+        }
+        static std::optional<muuid::uuid> fromDataStream(QDataStream& in) {
+            muuid::uuid id{};
+            const int bytesRead = in.readRawData(reinterpret_cast<char*>(id.bytes.data()), 16);
+
+            if (bytesRead == 16) return id;
+
+            in.setStatus(QDataStream::ReadPastEnd);
+            return std::nullopt;
+        }
     };
 }
