bcabee239360c863931b613d65c33a39eeb44849
Olivia Heartfelt <heartfelt.olivia@gmail.com>
2026-09-09 00:19:01 +0200

    Refactor streaming manager

--- src/editor/ANodeEnvironment/Streaming/STStreamingManager.h
+++ src/editor/ANodeEnvironment/Streaming/STStreamingManager.h
@@ -27,55 +27,54 @@ namespace STStreamingManager {
 
             ANodeEnvDB::ANodeEnvDB* m_db = nullptr;
 
-        STLoadingNodeConsumer::STLoadingNodeConsumer* m_loadingNodeConsumer = nullptr;
-        STLoadingNodeStreamer::STLoadingNodeStreamer* m_loadingNodeStreamer = nullptr;
+        std::unique_ptr<STStreamingManager::LoadingNodeConsumer::STLoadingNodeConsumer> m_loadingNodeConsumer;
+        std::unique_ptr<STStreamingManager::LoadingNodeStreamer::STLoadingNodeStreamer> m_loadingNodeStreamer;
 
-        STLoadingWireConsumer::STLoadingWireConsumer* m_loadingWireConsumer = nullptr;
-        STLoadingWireStreamer::STLoadingWireStreamer* m_loadingWireStreamer = nullptr;
+        std::unique_ptr<STStreamingManager::LoadingWireConsumer::STLoadingWireConsumer> m_loadingWireConsumer;
+        std::unique_ptr<STStreamingManager::LoadingWireStreamer::STLoadingWireStreamer> m_loadingWireStreamer;
 
-        STSavingConsumer::STSavingConsumer* m_savingConsumer = nullptr;
-        STSavingStreamer::STSavingStreamer* m_savingStreamer = nullptr;
+        std::unique_ptr<STStreamingManager::SaveConsumer::STSavingConsumer> m_savingConsumer;
+        std::unique_ptr<STStreamingManager::SaveStreamer::STSavingStreamer> m_savingStreamer;
 
         QFuture<void> m_currentLoadingStreamerFuture;
         QFuture<void> m_currentSavingStreamerFuture;
 
     public:
-        explicit StreamingManager(
-            ANodeEnvDB::ANodeEnvDB* db,
-            STLoadingNodeConsumer::STLoadingNodeConsumer* loadingNodeConsumer,
-            STLoadingNodeStreamer::STLoadingNodeStreamer* loadingNodeStreamer,
-            STLoadingWireConsumer::STLoadingWireConsumer* loadingWireConsumer,
-            STLoadingWireStreamer::STLoadingWireStreamer* loadingWireStreamer,
-            STSavingConsumer::STSavingConsumer* savingConsumer,
-            STSavingStreamer::STSavingStreamer* savingStreamer,
-            QObject* parent = nullptr
-        ) :
-            QObject(parent),
-            m_db(db),
-            m_loadingNodeConsumer(loadingNodeConsumer),
-            m_loadingNodeStreamer(loadingNodeStreamer),
-            m_loadingWireConsumer(loadingWireConsumer),
-            m_loadingWireStreamer(loadingWireStreamer),
-            m_savingConsumer(savingConsumer),
-            m_savingStreamer(savingStreamer)
-        {
+        explicit StreamingManager(ANodeEnvDB::ANodeEnvDB* db, QGraphicsScene* scene, ARegistry::Registry& m_registry, QObject* parent = nullptr) : m_db(db), QObject(parent) {
+            if (!scene || !m_db) return;
+
+            m_loadingNodeConsumer = std::make_unique<STStreamingManager::LoadingNodeConsumer::STLoadingNodeConsumer>(scene, m_db, &m_registry, this);
+            m_loadingNodeStreamer = std::make_unique<STLoadingNodeStreamer::STLoadingNodeStreamer>(m_db, &m_registry);
+            
+            m_loadingWireConsumer = std::make_unique<STStreamingManager::LoadingWireConsumer::STLoadingWireConsumer>(scene, m_db, &m_registry, this);
+            m_loadingWireStreamer = std::make_unique<STLoadingWireStreamer::STLoadingWireStreamer>(m_db, &m_registry);
+
+            m_savingConsumer = std::make_unique<STStreamingManager::SaveConsumer::STSavingConsumer>(m_db, this);
+            m_savingStreamer = std::make_unique<STStreamingManager::SaveStreamer::STSavingStreamer>(m_db, &m_registry);
+
             if (m_loadingNodeConsumer) {
-                connect(m_loadingNodeConsumer, &STLoadingNodeConsumer::STLoadingNodeConsumer::nodesLoadingFinished,
+                connect(m_loadingNodeConsumer.get(), &STLoadingNodeConsumer::STLoadingNodeConsumer::nodesLoadingFinished,
                     this, &StreamingManager::onNodesPhaseFinished);
             }
             if (m_loadingWireConsumer) {
-                connect(m_loadingWireConsumer, &STLoadingWireConsumer::STLoadingWireConsumer::wiresLoadingFinished,
+                connect(m_loadingWireConsumer.get(), &STLoadingWireConsumer::STLoadingWireConsumer::wiresLoadingFinished,
                     this, &StreamingManager::loadFinished);
             }
             if (m_savingConsumer) {
-                connect(m_savingConsumer, &STSavingConsumer::STSavingConsumer::saveFinished,
+                connect(m_savingConsumer.get(), &STSavingConsumer::STSavingConsumer::saveFinished,
                     this, &StreamingManager::saveFinished);
             }
         }
-
         ~StreamingManager() override {
             cancelCurrentLoad();
             cancelCurrentSave();
+
+            m_loadingNodeConsumer.reset();
+            m_loadingNodeStreamer.reset();
+            m_loadingWireConsumer.reset();
+            m_loadingWireStreamer.reset();
+            m_savingConsumer.reset();
+            m_savingStreamer.reset();
         }
 
         void load() {
@@ -123,6 +122,22 @@ namespace STStreamingManager {
             if (m_currentSavingStreamerFuture.isRunning()) {
                 m_currentSavingStreamerFuture.waitForFinished();
             }
+        }
+
+        bool isValid() const {
+            return
+                m_loadingNodeConsumer &&
+                m_loadingNodeStreamer &&
+                m_loadingWireConsumer &&
+                m_loadingWireStreamer &&
+                m_savingConsumer &&
+                m_savingStreamer;
+        }
+        bool isSaving() const {
+            return m_currentSavingStreamerFuture.isRunning();
+        }
+        bool isLoading() const {
+            return m_currentLoadingStreamerFuture.isRunning();
         }
 
     signals:
